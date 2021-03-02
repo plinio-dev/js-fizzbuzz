a209aff6735d5494137da109e60b423f8e24e303
Tommaso P <tommaso.plinio@gmail.com>
2021-03-02 15:32:51 +0100

    start

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/* Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
+
+
+//1 scrivere i numeri da 1 a 100
+
+//2 stampare Fizz al posto dei multipli di 3
+
+//3 stampare Buzz al posto dei multipli di 5
+
+//4 stampare FizzBuzz al posto dei numeri sia multipli di 3 che di 5
