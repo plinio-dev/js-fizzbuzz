966d24b8bcd116403263f4f75d3cac757079ace2
Tommaso P <tommaso.plinio@gmail.com>
2021-03-02 17:58:54 +0100

    Fixed

--- js/script.js
+++ js/script.js
@@ -9,18 +9,17 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz. */
 for (var i=1; i <= 100; i++) {
 
   //4 stampare FizzBuzz al posto dei numeri sia multipli di 3 che di 5
-  if ( i %3 == 0  &&  i %5 == 0)   {
+  if ( (i %3 == 0)  &&  (i %5 == 0) ) {
     console.log("FizzBuzz");
   }
-    //2 stampare Fizz al posto dei multipli di 3
-    else {
-      if ( i %3 == 0 ) {
-        console.log("Fizz");
-      }
-        //3 stampare Buzz al posto dei multipli di 5
-        if ( i %5 == 0 ) {
-          console.log("Buzz");
-        }
-    }
+  //2 stampare Fizz al posto dei multipli di 3
+  else if ( i %3 == 0 ) {
+      console.log("Fizz");
+  }
+  //3 stampare Buzz al posto dei multipli di 5
+  else if ( i %5 == 0 ) {
+      console.log("Buzz");
+  } else {
     console.log([i]);
+  }
 }
